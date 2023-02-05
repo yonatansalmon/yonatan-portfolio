9fcaa40189773788d0dac3059832fa11d645a0e6
Yonatan Salmon <yonatansalmon@itc.tech>
2023-02-05 17:15:31 +0200

    parallax scroll

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import Nav from './Nav';
-import { Outlet } from 'react-router-dom';
-import SideBar from './SideBar';
+// import React from 'react';
+// import Nav from './Nav';
+// import { Outlet } from 'react-router-dom';
+// import SideBar from './SideBar';
 
-export const Root = () => {
-  return (
-    <>
-      <SideBar />
-      <Outlet />
-    </>
-  );
-};
+// export const Root = () => {
+//   return (
+//     <>
+//       <SideBar />
+//       <Outlet />
+//     </>
+//   );
+// };
