6c12ed28ee977a92669a22ba0245fccba52c8451
Yonatan Salmon <yonatansalmon@itc.tech>
2023-02-06 13:35:07 +0200

    Deploy

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,69 @@
+import { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+
+const ContactForm = () => {
+  const [contactInfo, setContactInfo] = useState({ name: '', email: '', body: '' });
+  const [error, setError] = useState(false);
+
+  const form = useRef();
+
+  const handleFormInput = (e) => {
+    setContactInfo({ ...contactInfo, [e.target.name]: e.target.value });
+  };
+
+  const notify = () =>
+    toast('Thank you for reaching out!', {
+      position: 'bottom-right',
+      className: 'Toast',
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'light',
+    });
+
+  const handleContact = async (e) => {
+    e.preventDefault();
+    setError(false);
+    try {
+      const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+      if (contactInfo.name === '') {
+        setError('Please enter a name');
+        return;
+      }
+      if (!contactInfo.email || regex.test(contactInfo.email) === false) {
+        setError('Invalid Email');
+        return;
+      }
+      if (contactInfo.body === '') {
+        setError('Please enter a message for the email');
+        return;
+      }
+      const res = await emailjs.sendForm('service_pcizvve', 'template_5mfv9y4', form.current, 'kaO5fDd6_HAzMTs25');
+      if (res.status === 200) {
+        notify();
+      }
+    } catch (err) {
+      console.log(err);
+    }
+    setContactInfo({ name: '', email: '', body: '' });
+  };
+  return (
+    <form className='ContactForm' ref={form} onSubmit={handleContact}>
+      <h4>Send a Note</h4>
+      <div className='NameEmailContainer'>
+        <input type='text' name='name' placeholder='Name' className='Name FormInput' onChange={handleFormInput} value={contactInfo.name} />
+        <input type='text' name='email' placeholder='Email' onChange={handleFormInput} value={contactInfo.email} className='Email FormInput' />
+        {error && <h1 className='ErrorMessage'>{error}</h1>}
+      </div>
+      <textarea className='FormInput' name='body' onChange={handleFormInput} value={contactInfo.body} placeholder='Tell me more...'></textarea>
+      <button className='SendMsgBtn'>Send Message </button>
+    </form>
+  );
+};
+
+export default ContactForm;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,69 +5,56 @@ import { ReactComponent as EmailIcon } from '../icons/EmailIcon.svg';
 import { ReactComponent as GithubIcon } from '../icons/GithubIcon.svg';
 import { ReactComponent as LinkedinIcon } from '../icons/LinkedinIcon.svg';
 import { ReactComponent as PhoneIcon } from '../icons/PhoneIcon.svg';
-import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
 import { useState, useRef } from 'react';
-
+import '../App.css';
+import ContactForm from '../components/ContactForm';
 
 const Contact = (props) => {
-  const [contactInfo, setContactInfo] = useState({name: '', email: '', body: ''});
-  const form = useRef();
-
-  const handleFormChange = (e) => {
-    console.log(e.target.value)
-    setContactInfo(e.target.value)
-  };
-
-  const handleContact = async (e) => {
-    e.preventDefault();
-    console.log(contactInfo)
-    try {
-      const res = await emailjs.sendForm('service_pcizvve', 'template_5mfv9y4', form.current, 'kaO5fDd6_HAzMTs25');
-      console.log(res)
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   return (
-    <section className='contactContainer'>
-      <Title backgroundTitle='Contact' frontalTitle='Get In Touch' />
-      <div className='ContactInfoContainer'>
-        <div className='ContactInfo'>
-          <div className='ContactDetails'>
-            <HomeIcon className='Icon' />
-            Petach Tikva, Israel
+    <>
+      <section className='contactContainer'>
+        <Title backgroundTitle='Contact' frontalTitle='Get In Touch' />
+        <div className='ContactInfoContainer'>
+          <div className='ContactInfo'>
+            <div className='ContactDetails'>
+              <HomeIcon className='Icon' />
+              Petach Tikva, Israel
+            </div>
+            <div className='ContactDetails'>
+              <PhoneIcon className='Icon' />
+              142141241
+            </div>
+            <div className='ContactDetails'>
+              <EmailIcon className='Icon' />
+              yonatansalmon_@gmail.com
+            </div>
+            <h4 className='FollowMe'>Follow Me</h4>
+            <div className='IconContainer'>
+              <a className='LinkIcon' rel='noreferrer' href='https://www.linkedin.com/in/yonatansalmon' target='_blank'>
+                <LinkedinIcon className='Icon' />
+              </a>
+              <a className='LinkIcon' rel='noreferrer' href='https://www.linkedin.com/in/yonatansalmon' target='_blank'>
+                <GithubIcon className='Icon' />
+              </a>
+            </div>
           </div>
-          <div className='ContactDetails'>
-            <PhoneIcon className='Icon' />
-            142141241
-          </div>
-          <div className='ContactDetails'>
-            <EmailIcon className='Icon' />
-            yonatansalmon_@gmail.com
-          </div>
-          <h4 className='FollowMe'>Follow Me</h4>
-          <div className='IconContainer'>
-            <a className='LinkIcon' rel='noreferrer' href='https://www.linkedin.com/in/yonatansalmon' target='_blank'>
-              <LinkedinIcon className='Icon' />
-            </a>
-            <a className='LinkIcon' rel='noreferrer' href='https://www.linkedin.com/in/yonatansalmon' target='_blank'>
-              <GithubIcon className='Icon' />
-            </a>
-          </div>
+          <ContactForm />
         </div>
-
-        <form className='ContactForm' ref={form} onSubmit={handleContact}>
-          <h4>Send a Note</h4>
-          <div className='NameEmailContainer'>
-            <input type='text' name='name' placeholder='Name' onChange={handleFormChange} className='Name FormInput' />
-            <input type='text' name='email' placeholder='Email' onChange={handleFormChange} className='Email FormInput' />
-          </div>
-          <textarea className='FormInput' name='body' onChange={handleFormChange} placeholder='Tell me more...'></textarea>
-          <button className='SendMsgBtn'>Send Message </button>
-        </form>
-      </div>
-    </section>
+      </section>
+      <ToastContainer
+        position='bottom-right'
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='light'
+      />
+    </>
   );
 };
 
