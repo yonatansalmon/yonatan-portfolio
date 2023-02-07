e04cd642daabf7ef9aa4c2faf7fffe1518f77b87
Yonatan Salmon <yonatansalmon@itc.tech>
2023-02-07 17:47:33 +0200

    Mobile UX

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from './Nav'
+import { useNavigate } from 'react-router-dom'
 
 const SideBar = ({parallax}) => {
+  const navigate = useNavigate()
   return (
     <div className='SideBar'>
-      <img className='ProfilePic' src='https://res.cloudinary.com/dm4rue7fk/image/upload/v1673872169/yonatan_ozuwpw.jpg' alt='yonatan'></img>
+      <img className='ProfilePic' onClick={() => navigate('/')} src='https://res.cloudinary.com/dm4rue7fk/image/upload/v1673872169/yonatan_ozuwpw.jpg' alt='yonatan'></img>
      <Nav parallax={parallax}/>
     </div>
   )
